038c0c455f1247c7067114b3d5c94d9ad8c3bb72
Guudz <Kilian1702@outlook.com>
2023-02-21 15:46:59 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version "cerulean"
+
+game "gta5"
+
+lua54 "yes"
+
+this_is_a_map "yes"
+
+dependencies {"cfx-gabz-davispd"}
+
+server_scripts {
+    "@mysql-async/lib/MySQL.lua",
+    "sync/genpi1"
+}
